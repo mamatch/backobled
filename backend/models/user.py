ef75fc833163a162909534a8a00a9bd84d7b4bfb
Eteil Djoumatchoua <eteil.djoumatchoua@cluepoints.com>
2022-03-10 15:55:21 +0100

    + user end point api

---
+++ backend/models/user.py
@@ -0,0 +1,26 @@
+from datetime import datetime
+
+from pydantic import BaseModel
+import enum
+from fastapi import Query
+
+
+class Role(enum.Enum):
+    admin: str = "admin"
+    personal: str = "personal"
+
+
+class TypeID(enum.Enum):
+    cni: str = "CNI"
+    passport: str = "passport"
+
+
+class User(BaseModel):
+    first_name: str
+    last_name: str
+    birthdate: datetime
+    id_type: TypeID
+    login: str
+    password: str
+    mail: str = Query(..., regex="^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+).([a-zA-Z]{2,5})$")
+    role: Role
--- backend/run.py
+++ backend/run.py
@@ -1,6 +1,40 @@
 from fastapi import FastAPI
 
+from models.user import User
+
 app = FastAPI()
+
+
+# End points for users
+@app.post("/user")
+async def post_user(user: User):
+    """
+    A function to create a new user
+    :param user: The user object containing the information
+    :return:
+    """
+    return {"request body": user}
+
+
+@app.get("/user")
+async def get_user_validation(password: str):
+    """
+    A function to know if a user exists
+    :param password: The password of the user
+    :return:
+    """
+    return {"parameter": password}
+
+
+@app.put("/user")
+async def modify_user(id: int, user: User):
+    """
+    A function to modify information about a user
+    :param id: The id of the user
+    :param user: The object
+    :return:
+    """
+    pass
 
 
 @app.get("/hello")
