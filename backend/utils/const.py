fc0f4aef03443e4b646b5d7ef0e6c0046c860df9
Eteil Djoumatchoua <eteil.djoumatchoua@cluepoints.com>
2022-03-12 11:23:57 +0100

    + jwt security

---
+++ backend/utils/const.py
@@ -0,0 +1,4 @@
+# Generate secret key with openssl rand -hex 32
+JWT_SECRET_KEY = "3bf1211acb8400f558e1799700ebdd15cecc4fc2934125372961582746c4aa86"
+JWT_ALGORITHM = "HS256"
+JWT_EXPIRATION_TIME_MINUTES = 60 * 24 * 5
